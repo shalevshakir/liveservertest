54a40fe93d05fcadd467fee5dc763aa283e6f50d
shalevshakir <shalevshakir@gmail.com>
2024-09-12 14:49:54 +0300

    first commit

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ cat=[{"name":"betty","age":20},{"name":"alex","age":21},{"name":"shadi","age":15
 @app.route('/')
 def hello():
     return cat
+
+@app.route('/test')
+def test():
+    return "hello"
+ 
  
 if __name__ == '__main__':
     app.run(debug=True)
